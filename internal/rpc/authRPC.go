ae0665c41347a9a2f271800c1b2aeefb6ff5b610
IvanVojnic <ivojnic44@gmail.com>
2023-02-23 13:05:12 +0300

    fix: user

--- internal/rpc/authRPC.go
+++ internal/rpc/authRPC.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"github.com/google/uuid"
+	"userMS/internal/utils"
 	"userMS/models"
 	pr "userMS/proto"
 )
@@ -13,6 +14,12 @@ type Auth interface {
 	UpdateRefreshToken(ctx context.Context, rt string, id uuid.UUID) error
 	GetUserByID(ctx context.Context, userID uuid.UUID) (models.User, error)
 	SignIn(ctx context.Context, user *models.User) error
+}
+
+// Tokens used to define at and rt
+type Tokens struct {
+	AccessToken  string `json:"access"`
+	RefreshToken string `json:"refresh"`
 }
 
 type UserServer struct {
@@ -31,4 +38,29 @@ func (s *UserServer) SignUp(ctx context.Context, req *pr.SignUpRequest) (*pr.Sig
 		return &pr.SignUpResponse{IsCreated: false}, fmt.Errorf("error while creating user")
 	}
 	return &pr.SignUpResponse{IsCreated: true}, nil
+}
+
+func (s *UserServer) SignIn(ctx context.Context, req *pr.SignUpRequest) (*pr.SignInResponse, error) {
+	password := req.GetPassword()
+	user := models.User{Name: req.GetName(), Password: password}
+	err := s.authR.SignIn(ctx, &user)
+	if err != nil {
+		return &pr.SignInResponse{At: "", Rt: ""}, fmt.Errorf("error while login user, %s", err)
+	}
+	if user.Password == password {
+		rt, errRT := utils.GenerateToken(user.ID, utils.TokenRTDuration)
+		if errRT != nil {
+			return &pr.SignInResponse{At: "", Rt: ""}, fmt.Errorf("error while generate rt, %s", err)
+		}
+		at, errAT := utils.GenerateToken(user.ID, utils.TokenATDuretion)
+		if errAT != nil {
+			return &pr.SignInResponse{At: "", Rt: ""}, fmt.Errorf("error while generate at, %s", err)
+		}
+		errUpdateRT := s.authR.UpdateRefreshToken(ctx, rt, user.ID)
+		if errUpdateRT != nil {
+			return &pr.SignInResponse{At: "", Rt: ""}, fmt.Errorf("error while set rt, %s", err)
+		}
+		return &pr.SignInResponse{At: at, Rt: rt}, nil
+	}
+	return &pr.SignInResponse{At: "", Rt: ""}, fmt.Errorf("error while loogin, wrong credentials %s", err)
 }
