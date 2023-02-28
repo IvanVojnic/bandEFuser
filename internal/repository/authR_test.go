ed4e0d3bdad115b1012e9e49298caaf28c137f07
IvanVojnic <ivojnic44@gmail.com>
2023-02-28 14:01:22 +0300

    fix: add tests for user auth

--- internal/repository/authR_test.go
+++ internal/repository/authR_test.go
@@ -1,1 +1,23 @@
 package repository
+
+import (
+	"context"
+	"github.com/stretchr/testify/require"
+	"testing"
+)
+
+// Test_GetFriends used to get friends
+func Test_SignUp(t *testing.T) {
+	ctx := context.Background()
+	repos := NewUserPostgres(db)
+	err := repos.SignUp(ctx, &testUserValidData)
+	require.NoError(t, err, "get friends error")
+}
+
+// Test_GetFriends used to get friends
+func Test_SignIn(t *testing.T) {
+	ctx := context.Background()
+	repos := NewUserPostgres(db)
+	err := repos.SignIn(ctx, &testUserValidData)
+	require.NoError(t, err, "get friends error")
+}
